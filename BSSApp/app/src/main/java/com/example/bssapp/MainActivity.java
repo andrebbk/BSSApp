c2d006d86b16d86b4ca7f8268547ebc5034895f3
André Silva <andre.f.m.silva@hotmail.com>
2022-02-08 10:09:13 +0000

    clear bugged menu

--- BSSApp/app/src/main/java/com/example/bssapp/MainActivity.java
+++ BSSApp/app/src/main/java/com/example/bssapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.bssapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    //Intent k = new Intent(MainActivity.this, MenuActivity.class);
+                    //startActivity(k);
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
